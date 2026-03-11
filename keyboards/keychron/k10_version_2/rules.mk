424f4d7062930f0da6cac2da8255ad69736d1065
Julien Cassette <jcassette@proton.me>
2026-03-11 16:26:47 +0100

    keychron: keep only k3 max and k3 v3

--- keyboards/keychron/k10_version_2/rules.mk
+++
@@ -1,6 +0,0 @@
-include keyboards/keychron/common/wireless/wireless.mk
-include keyboards/keychron/common/keychron_common.mk
-
-VPATH += $(TOP_DIR)/keyboards/keychron
-
-SRC += ./common/matrix.c
--- keyboards/keychron/k4_version_3/ansi/rgb/rules.mk
+++
@@ -1,2 +0,0 @@
-# This file intentionally left blank
-KEYCHRON_RGB_ENABLE=yes
